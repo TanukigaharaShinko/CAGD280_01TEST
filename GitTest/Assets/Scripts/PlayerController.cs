5dadf8ef9f907eba2756e8d4253894fe66525832
TanukigaharaShinko <89051123+TanukigaharaShinko@users.noreply.github.com>
2023-10-31 13:25:19 -0700

    added different script

--- GitTest/Assets/Scripts/PlayerController.cs
+++ GitTest/Assets/Scripts/PlayerController.cs
@@ -5,5 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Implement player movement
+    public float speed;
+    private Vector3 movePos;
+    private void MovePlayer()
+    {
 
+    }
+
+    private void Jump()
+    {
+
+    }
 }
